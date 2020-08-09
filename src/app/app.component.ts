98738b8d3fa884a478de0b8c6fb419d9ae53ff15
Sree Sai Chaitanya Nagarajugari <snagaraj@tibco.com>
2020-08-09 12:34:51 -0700

    create Cipher class and use it to implement 2-way data-binding for cipher form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Cipher } from "./cipher";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.less"],
 })
 export class AppComponent {
-  title = 'caesar-cipher';
+  title = "caesar-cipher";
+  phrase: string;
+  offset: number;
+
+  cipherModel = new Cipher(this.phrase, this.offset);
+  onSubmit() {
+    console.log("form submitted");
+  }
 }
---
+++ src/app/cipher.ts
@@ -0,0 +1,3 @@
+export class Cipher {
+  constructor(public phrase: string, public offset: number) {}
+}

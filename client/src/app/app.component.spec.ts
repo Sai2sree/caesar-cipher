f5416bd9fb0591e85ba1115d4ce996228c6e3c65
Sree Sai Chaitanya Nagarajugari <snagaraj@tibco.com>
2020-08-11 22:18:37 -0700

    fix unit tests of client.

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,31 +1,39 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from "@angular/platform-browser";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+      declarations: [AppComponent],
+      imports: [
+        BrowserModule.withServerTransition({ appId: "serverApp" }),
+        FormsModule,
+        HttpClientModule,
       ],
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'caesar-cipher'`, () => {
+  it(`should have as title 'caesar cipher'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('caesar-cipher');
+    expect(app.title).toEqual("caesar cipher");
   });
 
-  it('should render title', () => {
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('caesar-cipher app is running!');
+    expect(compiled.querySelector(".cc-title").textContent).toContain(
+      "Caesar Cipher"
+    );
   });
 });
--- client/src/app/ciphering.service.spec.ts
+++ client/src/app/ciphering.service.spec.ts
@@ -1,9 +1,22 @@
 import { TestBed } from "@angular/core/testing";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
 
 import { CipheringService } from "./ciphering.service";
 
 describe("CipheringService", () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [
+        BrowserModule.withServerTransition({ appId: "serverApp" }),
+        FormsModule,
+        HttpClientModule,
+      ],
+    })
+  );
 
   it("should be created", () => {
     const service: CipheringService = TestBed.get(CipheringService);
